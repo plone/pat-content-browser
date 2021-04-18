3a32e768985ff5ec4fd951cc4b86f9588d559327
Johannes Raggam <thetetet@gmail.com>
2021-04-18 23:27:12 +0200

    maint: Upgrade to Patternslib v4 final.

--- webpack.config.js
+++ webpack.config.js
@@ -1,95 +1,23 @@
+process.traceDeprecation = true;
 const path = require("path");
-const webpack_helpers = require("patternslib/webpack/webpack-helpers");
+const patternslib_config = require("@patternslib/patternslib/webpack/webpack.config.js");
 
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
+module.exports = async (env, argv) => {
+    const config = patternslib_config(env, argv);
 
-module.exports = (env) => {
-    return {
-        entry: {
-            bundle: "./bundle-config.js",
-        },
-        externals: [
-            {
-                window: "window",
-            },
-        ],
-        output: {
-            filename: "[name].js",
-            chunkFilename: "chunks/[name].[contenthash].js",
-            publicPath: "/dist",
-            path: path.resolve(__dirname, "dist"),
-        },
-        optimization: {
-            minimize: true,
-            minimizer: [
-                new TerserPlugin({
-                    include: /(\.min\.js$|bundle-vendors.js$)/,
-                    extractComments: false,
-                    terserOptions: {
-                        output: {
-                            comments: false,
-                        },
-                    },
-                }),
-            ],
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules\/(?!(patternslib)\/).*/,
-                    loader: "babel-loader",
-                },
-                {
-                    test: /\.*(?:html|xml)$/i,
-                    use: "raw-loader",
-                },
-                {
-                    test: /\.(?:sass|scss|css)$/i,
-                    use: [
-                        {
-                            loader: "style-loader",
-                            options: {
-                                insert: webpack_helpers.top_head_insert,
-                            },
-                        },
-                        "css-loader",
-                        "sass-loader",
-                    ],
-                },
-                {
-                    test: /\.(png|jpe?g|gif)$/i,
-                    use: "file-loader",
-                },
-                {
-                    test: /\.svg$/,
-                    loader: "svg-inline-loader",
-                },
-                {
-                    test: /\.svelte$/,
-                    use: "svelte-loader",
-                },
-            ],
-        },
-        resolve: {
-            alias: {
-                svelte: path.resolve("node_modules", "svelte"),
-            },
-            extensions: [".wasm", ".mjs", ".js", ".json", ".svelte"],
-            mainFields: ["svelte", "browser", "module", "main"],
-        },
-        devServer: {
-            port: "8000",
-            host: "0.0.0.0",
-        },
-        plugins: [
-            new CleanWebpackPlugin(),
-            new DuplicatePackageCheckerPlugin({
-                verbose: true,
-                emitError: true,
-            }),
-        ],
+    config.entry = {
+        bundle: path.resolve(__dirname, "bundle-config.js"),
     };
+    config.output.path = path.resolve(__dirname, "dist/");
+
+    // Correct moment alias
+    config.resolve.alias.moment = path.resolve(__dirname, "node_modules/moment"); // prettier-ignore
+
+    if (argv.mode === "production") {
+        // Also create minified bundles along with the non-minified ones.
+        config.entry["bundle.min"] = path.resolve(__dirname, "bundle-config.js"); // prettier-ignore
+        config.output.chunkFilename = "chunks/[name].[contenthash].min.js";
+    }
+
+    return config;
 };
