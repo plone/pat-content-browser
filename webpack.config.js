ee19e04492353a42c67c50a5db215af91a69c09e
Johannes Raggam <thetetet@gmail.com>
2021-06-15 23:50:34 +0200

    maint(webpack): Simplify webpack and make svelte webpack config reuseable.

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,15 @@
 process.traceDeprecation = true;
 const path = require("path");
 const patternslib_config = require("@patternslib/patternslib/webpack/webpack.config.js");
+const svelte_config = require("./webpack.svelte");
 
 module.exports = async (env, argv) => {
-    const config = patternslib_config(env, argv);
+    const config = svelte_config(env, argv, patternslib_config(env, argv));
 
     config.entry = {
         bundle: path.resolve(__dirname, "bundle-config.js"),
     };
     config.output.path = path.resolve(__dirname, "dist/");
-
-    // Correct moment alias
-    config.resolve.alias.moment = path.resolve(__dirname, "node_modules/moment"); // prettier-ignore
-
-    // Add Svelte support
-    config.module.rules.push({
-        test: /\.svelte$/,
-        use: "svelte-loader",
-    });
-    config.resolve.alias.svelte = path.resolve("node_modules", "svelte");
-    config.resolve.extensions = [".wasm", ".mjs", ".js", ".json", ".svelte"];
-    config.resolve.mainFields = ["svelte", "browser", "module", "main"];
-
-    if (argv.mode === "production") {
-        // Also create minified bundles along with the non-minified ones.
-        config.entry["bundle.min"] = path.resolve(__dirname, "bundle-config.js"); // prettier-ignore
-        config.output.chunkFilename = "chunks/[name].[contenthash].min.js";
-    }
 
     return config;
 };
